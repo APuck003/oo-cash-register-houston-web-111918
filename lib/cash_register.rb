447f19a797c72a7c8d3a91230196191c6ab69297
Austin <Austin_MacBook@austins-mbp.attlocal.net>
2018-12-03 00:18:56 -0600

    first commit

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,37 @@
+# require 'pry'
+
+class CashRegister
+  attr_accessor :total, :discount, :items
+  
+  # @@all = []
+  
+  def initialize(discount=0)
+    @total = 0
+    @discount = discount
+  end
+  
+  def add_item(title, price, quantity=1)
+    self.total += price * quantity
+    self.items ||= []
+    quantity.times do
+      self.items << title
+    end
+    self.items
+    # @@all << title
+  end
+  
+  def apply_discount
+    if discount == 0
+      "There is no discount to apply."
+    else
+      @total -= total * (discount.to_f / 100)
+      "After the discount, the total comes to $#{total.to_i}."
+    end
+  end
+  
+  def void_last_transaction
+    self.total -= total
+  end
+  
+end
 
